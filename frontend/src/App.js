46c8fc6b1e03af4dcb3c57fd99a1bb9b604e502d
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2021-04-03 13:03:07 -0400

    create routes using react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import './App.css';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to my shop.</h1>
-    </>
+    <Router>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <Route exact path='/' component={HomeScreen} />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+      <Footer />
+    </Router>
   );
 }
 
