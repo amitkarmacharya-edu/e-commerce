f8d4f388fe8a1a8d7d0927925ac3cd2907ebc291
amit karmacharya <amit.education@gmail.com>
2021-04-04 19:49:48 -0400

    add flux bootstrap theme .min.css file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import store from './store/store';
-import './index.css';
+import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
