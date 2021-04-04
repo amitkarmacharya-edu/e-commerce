6ce1633dabc2faff0a7eb8e4f4929679d4c687ef
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2021-04-04 02:31:02 -0400

    create redux store for productsList and productDetails

--- frontend/src/store/actions/productActions.js
+++ frontend/src/store/actions/productActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import {
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_SUCCESS,
@@ -22,3 +25,22 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/product/${id}`);
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
