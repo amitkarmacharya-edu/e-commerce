03d54e4712b100ebc0bb103f9643bb0baa7fdb2b
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2021-04-04 02:43:24 -0400

    create routes using react router

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db.js')
-const products = require('./data/products')
+const productRoutes = require('./routes/productRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 dotenv.config()
 connectDB()
 const app = express();
 
+
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+app.use('/api/products', productRoutes);
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
