533b20c74b1aa46f3babc6e0de4c62912d07049e
amit karmacharya <amit.education@gmail.com>
2021-04-05 00:31:44 -0400

    create users routes and controller to authenticate users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
+const { response } = require("express");
 const expressAsyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const generateToken = require('../utils/generateTokens')
 
 // @desc Auth user & get tokens
 // @route POST /api/users/login
@@ -14,12 +16,31 @@ const authUser = expressAsyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
    } else {
      res.status(401)
      throw new Error('Invalid email or password')
    }
-});
+})
 
-module.exports = { authUser }
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access private
+const getUserProfile = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not Found')
+  }
+})
+
+module.exports = { authUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { authUser } = require('../controllers/userController')
+const { authUser, getUserProfile } = require('../controllers/userController')
+const protect = require('../middleware/authMiddleware')
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 module.exports = router
