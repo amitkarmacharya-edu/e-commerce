1795a2625753974369614ecffde0eccf1429b031
amit karmacharya <amit.education@gmail.com>
2021-04-05 18:47:34 -0400

    create redux store, reducer, constants and actions for login details

---
+++ frontend/src/store/actions/userActions.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userContants"
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type:  USER_LOGIN_REQUEST
+    })
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    )
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data
+    })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    })
+  }
+}
