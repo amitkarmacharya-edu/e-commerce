695b67408df532dfe8e5eed7317b5d840cdc9dfc
amit karmacharya <amit.education@gmail.com>
2021-04-04 23:03:08 -0400

    acreate cart screen and add routes to the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 import './App.css'
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Container>
             <Route exact path='/' component={HomeScreen} />
             <Route path='/product/:id' component={ProductScreen} />
+            <Route path='/cart/:id?' component={CartScreen} />
           </Container>
         </main>
       <Footer />
