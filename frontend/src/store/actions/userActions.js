0fdf6f4aa5c5868a84ca379d7d478f88ed0abbf5
amit karmacharya <amit.education@gmail.com>
2021-04-05 19:47:10 -0400

    use redux store, reducers, action and constants to login and logout users

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -1,38 +1,49 @@
-import axios from "axios"
-import { PRODUCT_LIST_FAIL } from "../constants/productConstants"
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userContants"
+import axios from "axios";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from "../constants/userContants";
 
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
-      type:  USER_LOGIN_REQUEST
-    })
+      type: USER_LOGIN_REQUEST,
+    });
 
     const config = {
       headers: {
-        'Content-Type': 'application/json'
-      }
-    }
+        "Content-Type": "application/json",
+      },
+    };
 
     const { data } = await axios.post(
-      '/api/users/login',
+      "/api/users/login",
       { email, password },
       config
-    )
+    );
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
-      payload: data
-    })
+      payload: data,
+    });
 
-    localStorage.setItem('userInfo', JSON.stringify(data))
+    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message
-    })
+          : error.message,
+    });
   }
-}
+};
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem("userInfo");
+    dispatch({
+      type: USER_LOGOUT,
+    })
+};
