cc84def35b737af012a7cc6d9ca0983c35ceffba
amit karmacharya <amit.education@gmail.com>
2021-04-06 12:20:54 -0400

    use redux constants, reducer, actions to save shipping address

--- frontend/src/screens/ShippingScreen/index.js
+++ frontend/src/screens/ShippingScreen/index.js
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../../components/FormContainer";
+import { savShippingAddress } from "../../store/actions/cartActions";
 
 const ShippingScreen = ({ history }) => {
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+  const dispatch = useDispatch()
+  const cart = useSelector(state => state.cart)
+  const { shippingAddress } = cart
+
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
 
   const submitHandler = (e) => {
     e.preventDefault()
+    dispatch(savShippingAddress({address, city, postalCode, country}))
   }
   return (
     <FormContainer>
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -21,8 +21,13 @@ const reducer = combineReducers({
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+
 const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const shippingAddressFromStorage = localStorage.getItem("shippinAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
   : null;
 
 const initialState = {
