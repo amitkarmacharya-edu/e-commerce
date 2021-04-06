9f9df19ba5f9e41aa9894952df142839d8faa988
amit karmacharya <amit.education@gmail.com>
2021-04-05 20:56:02 -0400

    create updateProfile route and controller to update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,4 +73,32 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
   }
 })
 
-module.exports = { authUser, getUserProfile, registerUser }
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access private
+const updateUserProfile = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if(req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not Found')
+  }
+})
+
+module.exports = { authUser, getUserProfile, registerUser, updateUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const { authUser, getUserProfile, registerUser } = require('../controllers/userController')
-const protect = require('../middleware/authMiddleware')
+const express = require("express");
+const router = express.Router();
+const {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} = require("../controllers/userController");
+const protect = require("../middleware/authMiddleware");
 
-router.route('/register').post(registerUser)
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route("/register").post(registerUser);
+router.post("/login", authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
-
-module.exports = router
+module.exports = router;
