fb7e12a7061f961efd16bb2dcfc5edbe9619db9d
amit karmacharya <amit.education@gmail.com>
2021-04-06 16:54:45 -0400

    create redux const, reducer, actions and store for creating order

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -12,6 +12,7 @@ import {
   userDetailReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
+import { orderCreateReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -21,6 +22,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
