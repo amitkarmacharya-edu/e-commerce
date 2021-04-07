ee7a13c9ad26fa94e6f1920ad1ca5e0f0771c2e8
amit karmacharya <amit.education@gmail.com>
2021-04-07 00:13:07 -0400

    create controller and routes for payment status

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -51,4 +51,28 @@ const getOrderById = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { addOrderItem, getOrderById };
+// @desc Updates order to paid
+// @route POST /api/orders/:id/pay
+// @access private
+const updateOrderToPaid = expressAsyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  
+  if(order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+
+    const updatedOrder = await Order.save()
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Payment Error')
+  }
+});
+
+module.exports = { addOrderItem, getOrderById, updateOrderToPaid };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { addOrderItem, getOrderById } = require('../controllers/orderController')
+const { addOrderItem, getOrderById, updateOrderToPaid } = require('../controllers/orderController')
 const protect = require("../middleware/authMiddleware");
 
 router.route('/').post(protect, addOrderItem)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 module.exports = router
