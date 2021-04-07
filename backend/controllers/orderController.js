ece1fad95250c89b1bdf0703cab072d2042f5f79
amit karmacharya <amit.education@gmail.com>
2021-04-07 01:46:19 -0400

    added paypal payment method

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -67,7 +67,7 @@ const updateOrderToPaid = expressAsyncHandler(async (req, res) => {
       email_address: req.body.payer.email_address
     }
 
-    const updatedOrder = await Order.save()
+    const updatedOrder = await order.save()
     res.json(updatedOrder)
   } else {
     res.status(404)
