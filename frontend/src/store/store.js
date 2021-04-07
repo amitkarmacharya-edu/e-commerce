ec89f7088e73f62abf4f1ad079b3c8b241cc8e76
amit karmacharya <amit.education@gmail.com>
2021-04-07 00:20:06 -0400

    create redux const, reducer and action for payment status

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -12,7 +12,7 @@ import {
   userDetailReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
-import { orderCreateReducer, orderDetailReducer } from './reducers/orderReducers'
+import { orderCreateReducer, orderDetailReducer, orderPayReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -24,6 +24,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
