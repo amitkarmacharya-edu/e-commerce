487803e919f0265ea9d2f1f0bc48f2916a658680
amit karmacharya <amit.education@gmail.com>
2021-04-08 21:35:48 -0400

    created uploads folder to save images, uploadRoutes upload images using multer

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,40 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db.js')
-const productRoutes = require('./routes/productRoutes')
-const userRoutes = require('./routes/userRoutes')
-const orderRoutes = require('./routes/orderRoutes')
-const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const express = require("express");
+const path = require('path')
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running in ${process.env.NODE_ENV} mode on port : ${PORT}`)
-})
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port : ${PORT}`
+  );
+});
