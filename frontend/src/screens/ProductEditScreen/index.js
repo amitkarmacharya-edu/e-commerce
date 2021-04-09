4ddab6706280e2b4be6b33cdbcbdcca8b046aae6
amit karmacharya <amit.education@gmail.com>
2021-04-08 21:13:29 -0400

    add edit functionality to the edit screen page

--- frontend/src/screens/ProductEditScreen/index.js
+++ frontend/src/screens/ProductEditScreen/index.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import FormContainer from '../../components/FormContainer'
-import { USER_EDIT_RESET } from '../../store/constants/userContants'
-import { listProductDetails, updateProduct } from "../../store/actions/productActions";
+import FormContainer from "../../components/FormContainer";
+import { PRODUCT_UPDATE_RESET } from "../../store/constants/productConstants";
+import {
+  listProductDetails,
+  updateProduct as editProduct,
+} from "../../store/actions/productActions";
 
 const UserEditScreen = ({ match, history }) => {
   const productId = match.params.id;
-  
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [image, setImage] = useState("");
@@ -20,28 +23,51 @@ const UserEditScreen = ({ match, history }) => {
   const [description, setDescription] = useState("");
 
   const dispatch = useDispatch();
-  
+
   const productDetails = useSelector((state) => state.productDetails);
-  const { loading, error, product } = productDetails
+  const { loading, error, product } = productDetails;
+
+  const updateProduct = useSelector((state) => state.updateProduct);
+  const {
+    loading: loadingUpdate,
+    success: successUpdate,
+    error: errorUpdate,
+    product: updatedProduct,
+  } = updateProduct;
 
   useEffect(() => {
-
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
+      history.push("/admin/productList");
+    } else {
       if (!product || product._id !== productId) {
-        dispatch(listProductDetails(productId))
+        dispatch(listProductDetails(productId));
       } else {
-        setName(name)
-        setPrice(price)
-        setImage(image)
-        setBrand(brand)
-        setCategory(category)
-        setCountInStock(countInStock)
-        setDescription(description)
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setBrand(product.brand);
+        setCategory(product.category);
+        setCountInStock(product.countInStock);
+        setDescription(product.description);
       }
-
-  }, [dispatch, history, productId, product]);
+    }
+  }, [dispatch, history, productId, product, successUpdate]);
 
   const submitHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    dispatch(
+      editProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        countInStock,
+        category,
+        description,
+      })
+    );
   };
 
   return (
@@ -50,6 +76,8 @@ const UserEditScreen = ({ match, history }) => {
         Go Back
       </Link>
       <FormContainer>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         <h1>Edit Product</h1>
         {loading ? (
           <Loader />
@@ -82,7 +110,7 @@ const UserEditScreen = ({ match, history }) => {
               <Form.Control
                 type="text"
                 placeholder="Enter Image url"
-                checked={image}
+                value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
             </Form.Group>
@@ -95,30 +123,31 @@ const UserEditScreen = ({ match, history }) => {
                 onChange={(e) => setCountInStock(e.target.value)}
               ></Form.Control>
             </Form.Group>
-            <Form.Group controlId='brand'>
-            <Form.Label>Brand</Form.Label>
+            <Form.Group controlId="brand">
+              <Form.Label>Brand</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Brand Name"
-                checked={brand}
+                value={brand}
                 onChange={(e) => setBrand(e.target.value)}
               ></Form.Control>
-              </Form.Group>
+            </Form.Group>
             <Form.Group controlId="category">
+              <Form.Label>Category</Form.Label>
               <Form.Control
                 label="Category"
                 type="text"
                 placeholder="Enter Category Name"
-                checked={category}
+                value={category}
                 onChange={(e) => setCategory(e.target.value)}
               ></Form.Control>
-              </Form.Group>
+            </Form.Group>
             <Form.Group controlId="description">
-            <Form.Label>Description</Form.Label>
+              <Form.Label>Description</Form.Label>
               <Form.Control
-                type="areatext"
+                type="text"
                 placeholder="Enter Description Name"
-                checked={description}
+                value={description}
                 onChange={(e) => setDescription(e.target.value)}
               ></Form.Control>
             </Form.Group>
